d896ebaa75e05bc049a042f389b903d8944a393a
titas <titas.kucinskas-janevicius@stud.techin.lt>
2025-08-29 11:17:12 +0300

    Added controllers

--- back/demo/src/main/java/lt/techin/demo/model/Role.java
+++ back/demo/src/main/java/lt/techin/demo/model/Role.java
@@ -1,4 +1,38 @@
 package lt.techin.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String roleName;
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Role() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return roleName;
+    }
+
+    public void setName(String roleName) {
+        this.roleName = roleName;
+    }
+
+//    @Override
+//    public String getAuthority() {
+//        return roleName;
+//    }
 }
